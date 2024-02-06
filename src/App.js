c05806cee3b0f555576e767ad4a8d51a2d138af3
HARIHARAN-2128 <hariharanrajen21@gmail.com>
2024-02-06 12:54:33 +0530

    feat: Adding app.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <Counter
+      heading={"Counter App"}
+      description={
+        "this app includes the counter for increasing and decreasing the value"
+      }
+    />
+  );
+}
+
+export default App;
